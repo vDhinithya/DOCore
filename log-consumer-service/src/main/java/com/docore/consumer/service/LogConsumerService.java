bc44c616117aebb9f950071f67530f2e3752feb7
vDhinithya <dhinithya12@gmail.com>
2025-12-15 21:37:45 +0530

    trying to streamline trace ID

--- log-consumer-service/src/main/java/com/docore/consumer/service/LogConsumerService.java
+++ log-consumer-service/src/main/java/com/docore/consumer/service/LogConsumerService.java
@@ -2,18 +2,28 @@ package com.docore.consumer.service;
 
 import com.docore.consumer.entity.LogEvent;
 import com.docore.consumer.repository.LogRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Objects;
 
 @Service
 public class LogConsumerService {
     @Autowired
     private LogRepository logRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(LogConsumerService.class);
+
     @KafkaListener(topics = "docore-logs-topic", groupId = "docore-log-consumer-group")
-    public void consumedLog(LogEvent logEvent) {
-        System.out.println("Received Log: " + logEvent.getMessage());
+    public void consumedLog(LogEvent logEvent, @Header Map<String, Object> headers) {
+        //System.out.println("Received Log: " + logEvent.getMessage());
+        //log.info("kafka headers: {}",headers);
+        log.info("Received Log: {}",logEvent.getMessage());
 
         logRepository.save(logEvent);
     }
--- log-producer-service/src/main/java/com/docore/producer/service/KafkaProducerService.java
+++ log-producer-service/src/main/java/com/docore/producer/service/KafkaProducerService.java
@@ -1,5 +1,7 @@
 package com.docore.producer.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.docore.producer.entity.LogEvent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -7,8 +9,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducerService.class);
+
     private final KafkaTemplate<String, LogEvent> kafkaTemplate;
-    
+
     @Value("${docore.kafka.topic}")
     private String topicName;
 
@@ -18,8 +23,9 @@ public class KafkaProducerService {
     }
 
     public void sendLog(LogEvent event) {
+        log.info("Sending log to Kafka topic '{}':{}", topicName, event.getMessage());
         kafkaTemplate.send(topicName, event);
-        System.out.println("sent to kafka: "+ event.getMessage());
+        // System.out.println("sent to kafka: "+ event.getMessage());
     }
 
 }
