f432ac9e33ba7242c87fd99d4546df64e378fb62
vDhinithya <dhinithya12@gmail.com>
2026-01-13 16:10:55 +0530

    feat: Implement Resilience4j circuit breaker for elasticsearch

--- log-consumer-service/src/main/java/com/docore/consumer/service/LogConsumerService.java
+++ log-consumer-service/src/main/java/com/docore/consumer/service/LogConsumerService.java
@@ -2,6 +2,7 @@ package com.docore.consumer.service;
 
 import com.docore.consumer.entity.LogEvent;
 import com.docore.consumer.repository.LogRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +25,15 @@ public class LogConsumerService {
     //@KafkaListener(topics = "docore-logs-topic", groupId = "docore-log-consumer-group")
     //public void consumedLog(LogEvent logEvent, @Header Map<String, Object> headers) {
     @KafkaListener(topics = "${docore.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumedLog(@Payload LogEvent event, @Headers Map<String, Object> headers) {
-        //System.out.println("Received Log: " + logEvent.getMessage());
+    @CircuitBreaker(name = "elasticBreaker", fallbackMethod = "fallbackForElastic")
+    public void consumedLog(@Payload LogEvent event, @Header(value = "traceId", required = false) String traceId) {
+        if(traceId != null){}
+        log.info("processing log : {} | traceID : {}", event.getMessage(), traceId);
+
+            //System.out.println("Received Log: " + logEvent.getMessage());
         //log.info("kafka headers: {}",headers);
         //log.info("Received Log: {}",logEvent.getMessage());
-        log.info("Received Log: {} | TraceID: {}", event.getMessage(), headers.get("traceId"));
+        //log.info("Received Log: {} | TraceID: {}", event.getMessage(), headers.get("traceId"));
 //        if ("POISON_PILL".equals(event.getMessage())) {
 //            log.error("⚠ POISON PILL DETECTED! Simulating crash...");
 //            throw new RuntimeException("Boom! This is a forced crash to test DLQ.");
@@ -36,6 +41,10 @@ public class LogConsumerService {
         // TEST: NON-RETRYABLE EXCEPTION ---
 //        this was to simulate what will happen if we received an invalid message
         logRepository.save(event);
+    }
+    public void fallbackForElastic( LogEvent event, String traceId, Throwable t){
+        log.error("Circuit open: elsatic search is down , Cause {}", t.getMessage());
+        log.warn("[Fallback] droping logs to console: {}", event);
     }
 
 }
